eff6901f36230d1c6df09cfef1a2bef80cfa42d9
Nusrat Jahan Marjana <58365562+Nusrat15-12999@users.noreply.github.com>
2020-08-17 00:32:41 +0600

    Add files via upload

---
+++ binary_search.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,arr[50];
+
+    printf("Enter size:");
+    scanf("%d",&n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    int left,right,mid,key ;
+    left=0;
+    right=n-1;
+    mid=(left+right)/2;
+
+    printf("Enter Search Key:");
+    scanf("%d",&key);
+
+    while(left<=right)
+    {
+        if(key<arr[mid])
+        {
+            right=mid-1;
+        }
+         else if(key>arr[mid])
+        {
+            left=mid+1;
+        }
+        else if(key==arr[mid])
+        {
+            printf("Location is :%d",mid);
+            break;
+        }
+
+        //mid=(left+right)/2;
+        if(left>right)
+        {
+            printf("not found");
+        }
+    }
+    getch();
+}
